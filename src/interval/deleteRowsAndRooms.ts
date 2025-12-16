4a163ceb6e285ee74d7d31b29460d174933d8da5
Jimmy <56059993+jimmylam1@users.noreply.github.com>
2025-12-16 08:50:10 -0800

    fix foreign key issue

--- src/interval/deleteRowsAndRooms.ts
+++ src/interval/deleteRowsAndRooms.ts
@@ -4,21 +4,22 @@ import { dbConnect } from "../common/db/connect"
 import { LoungeQueue, Rooms, RoomsWithGuildId } from "../types/db"
 
 export async function deleteOldRowsAndRooms(client: Client) {
-    // delete rows after 24 hours
-    const threshold = Date.now() - 86400000
+    // delete rows after 25 hours. Originally 24, but add 1 hour to take into account that rooms are around 45m behind
+    const threshold = Date.now() - 90000000
 
     const queues = await dbConnect(async db => {
         return await db.fetchAll<LoungeQueue>("SELECT * FROM loungeQueue WHERE startTime < ? AND active = 0", [threshold])
     })
-    const rooms = await dbConnect(async db => {
-        return await db.fetchAll<Rooms>("SELECT * FROM rooms WHERE createdAt < ?", [threshold])
-    })
 
-    if (queues.length || rooms.length) {
+    if (queues.length) {
         const queueIds = queues.map(q => q.id)
+        const queueWhereClauses = queues.map(q => "queue = ?")
+        const queueIdWhereClauses = queues.map(q => "id = ?")
+
+        const rooms = await dbConnect(async db => {
+            return await db.fetchAll<Rooms>(`SELECT * FROM rooms WHERE ${queueWhereClauses.join(" OR ")}`, queueIds)
+        })
         const roomIds = rooms.map(r => r.roomChannelId)
-        const playerWhereClauses = queues.map(q => "queue = ?")
-        const queueWhereClauses = queues.map(q => "id = ?")
         const roomWhereClauses = rooms.map(r => "roomChannelId = ?")
 
         await deleteRoomsManager(client, threshold).catch(e => console.error(`deleteRowsAndRooms.ts deleteRoomsManager() failed ${e}`))
@@ -26,18 +27,14 @@ export async function deleteOldRowsAndRooms(client: Client) {
         let last = 0
         await dbConnect(async db => {
             // order is important due to foreign key constraints
-            if (queueIds.length) {
-                await db.execute(`DELETE FROM players WHERE ${playerWhereClauses.join(" OR ")}`, queueIds)
-            }
+            await db.execute(`DELETE FROM players WHERE ${queueWhereClauses.join(" OR ")}`, queueIds)
             last = 1
             if (roomIds.length) {
                 await db.execute(`DELETE FROM votes WHERE ${roomWhereClauses.join(" OR ")}`, roomIds)
                 await db.execute(`DELETE FROM rooms WHERE ${roomWhereClauses.join(" OR ")}`, roomIds)
             }
             last = 2
-            if (queueIds.length) {
-                await db.execute(`DELETE FROM loungeQueue WHERE ${queueWhereClauses.join(" OR ")}`, queueIds)
-            }
+            await db.execute(`DELETE FROM loungeQueue WHERE ${queueIdWhereClauses.join(" OR ")}`, queueIds)
         }).catch(e => console.error(`deleteRowsAndRooms.ts main delete failed at last=${last}`, e))
     }
 }
